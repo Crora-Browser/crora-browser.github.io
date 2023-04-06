6487a5b72668829d7ebcb4b2a053a5a9007e9500
pradosh-arduino <pradoshnathan@gmail.com>
2023-04-06 08:00:18 +0530

    Some minor changes for text typing

--- _layouts/typewriter.js
+++ _layouts/typewriter.js
@@ -2,7 +2,7 @@ var i = 0;
 var speed = 70; // speed of typing (in milliseconds)
 
 function typeWriter() {
-    var txt = document.getElementById("typewriter").textContent.trim();
+    var txt = 'Experience the Internet without boundaries with Crora Browser'
     if (i < txt.length) {
         document.getElementById("typewriter").innerHTML += txt.charAt(i);
         i++;
